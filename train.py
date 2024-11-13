eb632e60cd8a69cff2252abd426761e334366b62
toilaluan <tranthanhluan.nd@gmail.com>
2024-11-13 16:39:35 +0000

    update

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ trainer = Trainer(
     log_every_n_steps=5,
     check_val_every_n_epoch=1,
     logger=wandb_logger,
-    val_check_interval=0.5,
+    val_check_interval=1000,
     limit_val_batches=100,
     enable_checkpointing=True,
 )
